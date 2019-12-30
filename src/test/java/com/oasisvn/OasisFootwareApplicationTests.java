9869058b73602c2112ec1aa84586f11e027698a2
william9x <lehoangnam197@gmail.com>
2019-12-30 14:57:13 +0700

    Initialize api

---
+++ src/test/java/com/oasisvn/OasisFootwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasisvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisFootwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
