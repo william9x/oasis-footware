532508f754cf240cae5843ac06a71dca88e73346
quanganh283 <quanganh@byob.vn>
2020-01-02 02:30:33 +0700

    .

--- src/app/pages/create-product/create-product.component.ts
+++ src/app/pages/create-product/create-product.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-create-product",
@@ -7,35 +6,10 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ["./create-product.component.css"]
 })
 export class CreateProductComponent implements OnInit {
-  fileData: File = null;
-  previewUrl: any = null;
-  uploadedFilePath: string = null;
 
-  constructor(private http: HttpClient) {}
+
+  constructor() {}
 
   ngOnInit() {}
 
-  preview() {
-    // Show preview
-    let mimeType = this.fileData.type;
-    if (mimeType.match(/image\/*/) == null) {
-      return;
-    }
-
-    let reader = new FileReader();
-    reader.readAsDataURL(this.fileData);
-    reader.onload = _event => {
-      this.previewUrl = reader.result;
-    };
-  }
-
-  onSubmit() {
-    const formData: any = new FormData();
-    formData.append("file", this.fileData);
-    this.http.post("https://api.imgur.com/3/upload", formData).subscribe(res => {
-      console.log(res);
-      this.uploadedFilePath = res.data.filePath;
-      alert("SUCCESS !!");
-    });
-  }
 }
