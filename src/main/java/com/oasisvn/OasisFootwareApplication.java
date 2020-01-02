99a374c4d1c4c47e61e765ba8dd48b25aded1e17
william9x <lehoangnam197@gmail.com>
2020-01-02 07:01:13 +0700

    add CORS settings

--- src/main/java/com/oasisvn/OasisFootwareApplication.java
+++ src/main/java/com/oasisvn/OasisFootwareApplication.java
@@ -2,6 +2,10 @@ package com.oasisvn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class OasisFootwareApplication {
@@ -10,4 +14,13 @@ public class OasisFootwareApplication {
         SpringApplication.run(OasisFootwareApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*").allowedOrigins("*");
+            }
+        };
+    }
 }
