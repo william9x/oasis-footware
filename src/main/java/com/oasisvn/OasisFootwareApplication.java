228989232cf39ca5f70535cd331bf83b931db57d
william9x <lehoangnam197@gmail.com>
2020-01-03 11:24:00 +0700

    Update OasisFootwareApplication.java

--- src/main/java/com/oasisvn/OasisFootwareApplication.java
+++ src/main/java/com/oasisvn/OasisFootwareApplication.java
@@ -19,7 +19,7 @@ public class OasisFootwareApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedOrigins("*");
+                registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "DELETE");
             }
         };
     }
