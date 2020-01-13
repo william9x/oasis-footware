4471bb8f258b6ad582c30d4942c4f226dfe7f687
william9x <lehoangnam197@gmail.com>
2020-01-13 10:59:16 +0700

    Update OasisFootwareApplication.java

--- src/main/java/com/oasisvn/OasisFootwareApplication.java
+++ src/main/java/com/oasisvn/OasisFootwareApplication.java
@@ -2,6 +2,10 @@ package com.oasisvn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class OasisFootwareApplication {
@@ -9,4 +13,15 @@ public class OasisFootwareApplication {
     public static void main(String[] args) {
         SpringApplication.run(OasisFootwareApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
 }
